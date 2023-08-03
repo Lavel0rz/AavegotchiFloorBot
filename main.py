d45c90e639056ec86557a899daa0e151f769b3bd
Lavel0rz <85870602+Lavel0rz@users.noreply.github.com>
2023-08-03 15:15:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ contract3 = web3.eth.contract(address=address, abi=abi3)
 
 
 st.title('AArena')
-
+os.environ["REPORTLAB_GRAPHICS"] = "rlextra.graphics"
 st.markdown('''In this game, two Aavegotchis battle against each other using their six traits: NRG, AGG, SPK, BRN, EYES, and EYEC. Each trait has a value between 0 and 99 (can exceed with wearables).
 
 During the battle, a random trait is selected, and the Aavegotchi with the higher value in that trait becomes the attacker. However, the direction of the attack is also randomly determined. If the direction is 1, the Aavegotchi with the higher value in the selected trait is the attacker, while if the direction is 0, the Aavegotchi with the lower value in the selected trait is the attacker.
@@ -29,7 +29,6 @@ The damage dealt by the attack is calculated as the absolute difference between 
 The Aavegotchi that wins the battle is the one with remaining HP.''')
 
 owner = '0x39292E0157EF646cc9EA65dc48F8F91Cae009EAe'
-import numpy as np
 aa_ids = contract3.functions.allAavegotchisOfOwner(owner).call()
 ls_gotchis = []
 ls_traits = []
@@ -47,9 +46,9 @@ gotchi2 = st.selectbox('Select Gotchi2', ls_gotchis)
 traits = ls_traits[ls_gotchis.index(gotchi1)]
 traits2 = ls_traits[ls_gotchis.index(gotchi2)]
 st.write(traits, ls_names[ls_gotchis.index(gotchi1)])
-st.image(contract.functions.getAavegotchiSvg(ls_gotchis[ls_gotchis.index(gotchi1)]).call())
+st.write(contract.functions.getAavegotchiSvg(ls_gotchis[ls_gotchis.index(gotchi1)]).call(), unsafe_allow_html=True)
 st.write(traits2, ls_names[ls_gotchis.index(gotchi2)])
-st.image(contract.functions.getAavegotchiSvg(ls_gotchis[ls_gotchis.index(gotchi2)]).call())
+st.write(contract.functions.getAavegotchiSvg(ls_gotchis[ls_gotchis.index(gotchi2)]).call(), unsafe_allow_html=True)
 
 TRAITS = ('NRG', 'AGG', 'SPK', 'BRN', 'EYES', 'EYEC')
 
@@ -65,6 +64,7 @@ def calculate_damage(attacker_trait, defender_trait, direction):
     damage = abs(attacker_value - defender_value)
     return damage
 
+
 def display_round_results(df):
     st.write("### Battle Round Results")
     st.table(df)
@@ -77,6 +77,32 @@ def display_winner(winner, hp1, hp2):
         st.success(f"{gotchi2_name} WINS with {hp2} HP left")
     else:
         st.warning("TIE!")
+
+def overlay_health_bar(svg_str, hp, damage):
+    # Create health bar rectangle
+    total_hp = 150
+    attacker_health = max(0, hp) / total_hp
+    hp_after_damage = max(0, hp - damage) / total_hp
+
+    # Calculate the width of green and red parts
+    bar_width = 65
+    green_width = int(attacker_health * bar_width)
+    red_width = bar_width - green_width
+
+    # Create SVG for the health bar
+    health_bar_svg = f'''
+    <svg width="{bar_width}" height="10" xmlns="http://www.w3.org/2000/svg">
+        <rect x="0" y="0" width="{green_width}" height="10" style="fill:green"/>
+        <rect x="{green_width}" y="0" width="{red_width}" height="10" style="fill:red"/>
+    </svg>
+    '''
+
+    # Overlay health bar on top of the original SVG
+    combined_svg = f'<svg width="200" height="160" xmlns="http://www.w3.org/2000/svg">{svg_str}{health_bar_svg}</svg>'
+    return combined_svg
+
+# ... (rest of the code remains the same)
+
 
 
 def display_round(round_data):
@@ -93,62 +119,21 @@ def display_round(round_data):
     attacker_svg = contract.functions.getAavegotchiSvg(attacker_id).call()
     defender_svg = contract.functions.getAavegotchiSvg(defender_id).call()
 
-    # Convert SVG to PNG using cairosvg
-    attacker_png = svg_to_png(attacker_svg)
-    defender_png = svg_to_png(defender_svg)
+    # Overlay health bars on top of images
+    combined_attacker_svg = overlay_health_bar(attacker_svg, round_data['attacker_hp'], round_data['Damage'])
+    combined_defender_svg = overlay_health_bar(defender_svg, round_data['defender_hp'], round_data['Damage'])
 
-    # Overlay health bars
-    combined_attacker_image = overlay_health_bar(attacker_png, round_data['attacker_hp'], round_data['Damage'])
-    combined_defender_image = overlay_health_bar(defender_png, round_data['defender_hp'], round_data['Damage'])
-
-    # Resize images to make them smaller
-    image_size = (150, 150)
-    combined_attacker_image = combined_attacker_image.resize(image_size)
-    combined_defender_image = combined_defender_image.resize(image_size)
-
-    # Arrange images in columns using st.columns
+    # Display SVG images
     col1, col2 = st.columns(2)
     with col1:
-        st.image(combined_attacker_image, caption=round_data['attacker_name'], use_column_width=True)
+        st.image(combined_attacker_svg, use_column_width=True)
         st.write(f"HP: {round_data['attacker_hp']}")
     with col2:
-        st.image(combined_defender_image, caption=round_data['defender_name'], use_column_width=True)
+        st.image(combined_defender_svg, use_column_width=True)
         st.write(f"HP: {round_data['defender_hp']}")
 
     st.write("---")
 
-# Function to convert SVG to PNG using cairosvg
-def svg_to_png(svg_str):
-    png_bytes = cairosvg.svg2png(bytestring=svg_str)
-    return png_bytes
-
-# Function to overlay health bar on the image
-def overlay_health_bar(image_bytes, hp, damage):
-    with Image.open(io.BytesIO(image_bytes)) as img:
-        img = img.convert("RGBA")
-
-        # Create health bar image
-        health_bar_height = 10
-        total_hp = 150  # Total HP is 150 in this case
-        attacker_health = max(0, hp) / total_hp
-        hp_after_damage = max(0, hp - damage) / total_hp
-
-        health_bar_image = Image.new('RGBA', (150, health_bar_height), color='green')
-        health_bar_image.paste((255, 0, 0, 255), [0, 0, int(attacker_health * 150), health_bar_height])
-        health_bar_image.paste((0, 255, 0, 255), [int(attacker_health * 150), 0, int(hp_after_damage * 150), health_bar_height])
-
-        # Resize health bar image to match Aavegotchi image size
-        health_bar_image = health_bar_image.resize((img.width, health_bar_height))
-
-        # Convert images to arrays
-        np_img = np.array(img)
-        np_health_bar = np.array(health_bar_image)
-
-        # Overlay the health bar image on the Aavegotchi image using NumPy
-        np_combined_img = np.copy(np_img)
-        np_combined_img[:health_bar_height, :img.width] = np_health_bar
-
-        return Image.fromarray(np_combined_img)
 def duel(gotchi1_traits, gotchi2_traits, gotchi1_name, gotchi2_name):
     hp1 = 150
     hp2 = 150
